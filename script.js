78d605296196a2b1429b4a4a7abd93494fc112c4
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2025-08-27 03:43:36 +0600

    add heart-icon functionality to the website

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const buttons = document.querySelectorAll(".heart-icon");
+const heartIcons = document.querySelectorAll(".heart-icon");
+const heartIconText = document.querySelector("#heart-icon-text");
+
+for (let i = 0; i < heartIcons.length; i++) {
+    heartIcons[i].addEventListener("click", function () {
+        let count = parseInt(heartIconText.textContent);
+        heartIconText.textContent = count + 1;
+    });
+}
