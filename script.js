3363e54b09642da32a0b257d9d639bb40c6e0888
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2025-08-27 19:14:57 +0600

    add js functionality to call buttons

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const heartIcons = document.querySelectorAll(".heart-icon");
 const heartIconText = document.querySelector("#heart-icon-text");
 const copyButtons = document.querySelectorAll(".copy-button");
 const copyButtonText = document.querySelector("#copy-text");
+const callButtons = document.querySelectorAll(".call-Button");
+const historyContainer = document.querySelector(".history-container");
+const cardHistory = document.querySelector(".card-history");
+
 for (let i = 0; i < heartIcons.length; i++) {
     heartIcons[i].addEventListener("click", function () {
         let count = parseInt(heartIconText.textContent);
@@ -19,3 +23,35 @@ for (let i = 0; i < copyButtons.length; i++) {
         copyButtonText.textContent = count + 1;
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const callButtons = document.querySelectorAll(".call-button");
+
+    for (let i = 0; i < callButtons.length; i++) {
+        callButtons[i].addEventListener("click", function () {
+            const card = callButtons[i].closest(".bg-white");
+            const number = card
+                .querySelector(".mt-3.text-2xl.font-bold")
+                .textContent.trim();
+            const serviceName = card
+                .querySelector(".service-name-text")
+                .textContent.trim();
+            const now = new Date();
+            const localTime = now.toLocaleTimeString();
+            const newHistory = document.createElement("div");
+            newHistory.className =
+                "bg-gray-50 shadow-sm px-4 py-4 flex justify-between items-center rounded-lg left-section";
+            newHistory.innerHTML = `
+                <div>
+                    <p class="mandurai-font history-service-name">${serviceName}</p>
+                    <p class="history-number">${number}</p>
+                </div>
+                <div class="right-section">
+                    <p class="history-time">${localTime}</p>
+                </div>
+            `;
+            const cardHistory = document.querySelector(".card-history");
+            cardHistory.append(newHistory);
+        });
+    }
+});
